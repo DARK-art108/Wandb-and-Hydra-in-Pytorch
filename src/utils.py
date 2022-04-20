c28e5f35f0f59d3133921e22d1ef25823049086c
Ritesh Yadav <daydreamingguy941@gmail.com>
2022-04-20 20:06:33 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+''' set_seed for Torch API '''
 def set_seed(seed = 42):
     '''Sets the seed of the entire notebook so results are the same every time we run.
     This is for REPRODUCIBILITY.'''
@@ -10,4 +11,12 @@ def set_seed(seed = 42):
     torch.backends.cudnn.benchmark = False
     # Set a fixed value for the hash seed
     os.environ['PYTHONHASHSEED'] = str(seed)
+
+''' set_seed for TF '''
+def seed_it_all(seed=7):
+    """ Attempt to be Reproducible """
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
     
